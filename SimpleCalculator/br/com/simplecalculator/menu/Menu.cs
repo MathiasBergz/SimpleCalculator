e55ee4c7bb6135438a01d5ffa47eba8be0e6e44f
MathiasBergz <mathias15052004@gmail.com>
2024-10-02 20:46:57 -0300

    Fixed issue #20 and modified menu

--- SimpleCalculator/br/com/simplecalculator/menu/Menu.cs
+++ SimpleCalculator/br/com/simplecalculator/menu/Menu.cs
@@ -11,61 +11,78 @@ namespace SimpleCalculator.br.com.simplecalculator.menu
     {
         public static void Main(string[] args)
         {
-            Sum sum = new Sum();
-            Subtraction sub = new Subtraction();
-            Multiplication multi = new Multiplication();
-            Division divi = new Division();
-            int op;
+            Menu menu = new Menu();
             Console.WriteLine("Hello");
             Console.WriteLine("This program is a simple calculator, that can operate with two numbers");
             Console.WriteLine("Press enter to continue");
             Console.ReadLine();
             Console.Clear();
-            do
-            {
-                Console.WriteLine("Type the option you want to operate");
-                Console.WriteLine("======================================");
-                Console.WriteLine("= 1) Sum                             =");
-                Console.WriteLine("= 2) Subtraction                     =");
-                Console.WriteLine("= 3) Multiplication                  =");
-                Console.WriteLine("= 4) Division                        =");
-                Console.WriteLine("= 0) Exit                            =");
-                Console.WriteLine("======================================");
-
-                op = int.Parse(Console.ReadLine());
-                switch (op)
-                {
-                    case 0:
-                        Environment.Exit(0);
-                        break;
-                    case 1:
-                        sum.SumMenu();
-                        sum.WriteResult();
-                        Environment.Exit(0);
-                        break;
-                    case 2:
-                        sub.SubtractionMenu();
-                        sub.WriteResult();
-                        Environment.Exit(0);
-                        break;
-                    case 3:
-                        multi.MultiplicationMenu();
-                        multi.WriteResult();
-                        Environment.Exit(0);
-                        break;
-                    case 4:
-                        divi.DivisionMenu();
-                        divi.WriteResult();
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Option not available, try again");
-                        Console.ReadLine();
-                        Console.Clear();
-                        break;
-                }
-            } while (op > 0);
+            menu.MainMenu();
         }
+        
+        public void MainMenu()
+        {
+            Sum sum = new Sum();
+            Subtraction sub = new Subtraction();
+            Multiplication multi = new Multiplication();
+            Division divi = new Division();
+            try
+            {
+                int op;
+                do
+                {
+                    Console.WriteLine("Type the option you want to operate");
+                    Console.WriteLine("======================================");
+                    Console.WriteLine("= 1) Sum                             =");
+                    Console.WriteLine("= 2) Subtraction                     =");
+                    Console.WriteLine("= 3) Multiplication                  =");
+                    Console.WriteLine("= 4) Division                        =");
+                    Console.WriteLine("= 0) Exit                            =");
+                    Console.WriteLine("======================================");
 
+                    op = int.Parse(Console.ReadLine());
+                    switch (op)
+                    {
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+                        case 1:
+                            sum.SumMenu();
+                            sum.WriteResult();
+                            Environment.Exit(0);
+                            break;
+                        case 2:
+                            sub.SubtractionMenu();
+                            sub.WriteResult();
+                            Environment.Exit(0);
+                            break;
+                        case 3:
+                            multi.MultiplicationMenu();
+                            multi.WriteResult();
+                            Environment.Exit(0);
+                            break;
+                        case 4:
+                            divi.DivisionMenu();
+                            divi.WriteResult();
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Option not available, try again");
+                            Console.WriteLine();
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                    }
+                } while (op > 0);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine($"Type only words. Please try again. Error: {fe.Message}");
+                Console.WriteLine("Press enter to continue");
+                Console.ReadLine();
+                Console.Clear();
+                this.MainMenu();
+            }
+        }
     }
 }
